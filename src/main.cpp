18a9c27a560831eff4a2dcc984f40a993f02b067
hendo00 <ahmeduk0022@gmail.com>
2025-12-18 17:49:08 +0300

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-
 #include <MCP342x.h>
 #include <Wire.h>
 #include <micro_ros_platformio.h>
@@ -16,21 +15,12 @@
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
 // 0x68 is the default address for all MCP342x devices
 uint8_t address = 0x68;
 MCP342x adc = MCP342x(address);
 float tension;
-// Configuration settings
-MCP342x::Config config(MCP342x::channel1, MCP342x::oneShot,
-		       MCP342x::resolution16, MCP342x::gain2);
 
-// Configuration/status read back from the ADC
-MCP342x::Config status;
-
-// Inidicate if a new conversion should be started
-bool startConversion = false;
-
-long val=0;
 // ROS Configuration
 rcl_publisher_t publisher;
 std_msgs__msg__Float32 msg;
@@ -38,7 +28,6 @@ rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
-rcl_node_options_t node_ops;
 rcl_timer_t timer;
 
 // micro-ROS Agent Connection States
@@ -51,19 +40,20 @@ enum AgentState {
 
 AgentState agent_state = WAITING_AGENT;
 
+// Timing variables
+unsigned long last_read_time = 0;
+const unsigned long READ_INTERVAL = 0.5; // ms (target 50ms, leave 5ms for ROS processing)
+long val = 0;
+
 // Function to map float values
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-
-
 // Error handling loop
 void error_loop() {
   while (1) {
-  
-   
     delay(100);
   }
 }
@@ -84,12 +74,10 @@ bool create_ros_entities()
   const char * ns = "";
 
   allocator = rcl_get_default_allocator();
-//   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
-RCCHECK(rcl_init_options_init(&init_options, allocator));
-RCCHECK(rcl_init_options_set_domain_id(&init_options, 107));  
-
-RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+  rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+  RCCHECK(rcl_init_options_init(&init_options, allocator));
+  RCCHECK(rcl_init_options_set_domain_id(&init_options, 107));  
+  RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
 
   RCCHECK(rclc_node_init_default(&node, node_name, ns, &support));
 
@@ -99,7 +87,7 @@ RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &alloca
       ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
       "winch0/force_sensor_data"));
 
-  const unsigned int timer_timeout = 500;
+  const unsigned int timer_timeout = 0.5;  // 20Hz
   RCCHECK(rclc_timer_init_default(
       &timer,
       &support,
@@ -126,9 +114,9 @@ void destroy_ros_entities()
 void setup()
 {
     // Initialize Serial & micro-ROS Transport
-    Serial.begin(115200);
+    Serial.begin(2000000);
     Wire.begin();
-  
+    Wire.setClock(400000); // Increase I2C clock speed to 400kHz
 
     // Reset devices
     MCP342x::generalCallReset();
@@ -141,18 +129,13 @@ void setup()
         ;
     }
   
-    // First time loop() is called start a conversion
-    startConversion = true;
+    // Start a continuous conversion
+    MCP342x::Config config(MCP342x::channel1, MCP342x::continous,
+                         MCP342x::resolution16, MCP342x::gain2);
+    adc.configure(config);
+    
     set_microros_serial_transports(Serial);
     delay(2000);
-
-    // Serial.println("MCP3428 Analog to Digital Converter");
-
-    // // Initialize Display
-    // Serial.println("Initializing display...");
-
-
-    // Serial.println("Display initialized.");
 }
 
 void loop()
@@ -168,7 +151,6 @@ void loop()
         case AGENT_AVAILABLE:
             if (create_ros_entities()) {
                 agent_state = AGENT_CONNECTED;
-
             } else {
                 agent_state = WAITING_AGENT;
             }
@@ -178,24 +160,27 @@ void loop()
             if (rmw_uros_ping_agent(100, 1) != RMW_RET_OK) {
                 agent_state = AGENT_DISCONNECTED;
             } else {
-                rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+                rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)); // Reduced timeout
             }
             break;
 
         case AGENT_DISCONNECTED:
             destroy_ros_entities();
-
             agent_state = WAITING_AGENT;
             break;
     }
 
-    MCP342x::Config status;
-    // Initiate a conversion; convertAndRead() will wait until it can be read
-    uint8_t err = adc.convertAndRead(MCP342x::channel1, MCP342x::oneShot,
-                     MCP342x::resolution16, MCP342x::gain2,
-                     1000000, val, status);
-    
-    tension = mapfloat(constrain(val, 5619, 24700), 5619, 24700, 0.0, 100.0); // Map the value to a float range
-    
-    delay(500);
+    // Read ADC at 20Hz rate
+    unsigned long current_time = millis();
+    if (current_time - last_read_time >= READ_INTERVAL) {
+        last_read_time = current_time;
+        
+        // Read the value directly without reconfiguring (since we're in continuous mode)
+        MCP342x::Config status;
+        uint8_t err = adc.read(val, status);
+        
+        if (err == 0) { // Success
+            tension = mapfloat(constrain(val, 5619, 24700), 5619, 24700, 0.0, 100.0);
+        }
+    }
 }
